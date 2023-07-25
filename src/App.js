71e5bce19f0cdb309c6746055ea809f9c5666255
joeburnsJB <126894305+joeburnsJB@users.noreply.github.com>
2023-07-25 04:19:25 +0100

    trying to fix blankpage

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { HashRouter, Routes, Route} from "react-router-dom";
+import { BrowserRouter, Routes, Route} from "react-router-dom";
 import './App.css';
 import Homepage from './pages/homepage/homepage';
 import Caloric from './pages/caloric/caloric';
@@ -6,12 +6,12 @@ import Caloric from './pages/caloric/caloric';
 const App = () => {
   return (
     <>
-      <HashRouter>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
         <Routes>
           <Route path="/" element={<Homepage />} />
           <Route path="/caloric" element={<Caloric />} />
         </Routes>
-      </HashRouter>
+      </BrowserRouter>
     </>
   );
 }
